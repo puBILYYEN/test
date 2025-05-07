1f9bf0f31fa88a0d4480d079d26f31445b83591d
顏仁駿 <144759165+puBILYYEN@users.noreply.github.com>
2025-05-07 16:59:28 +0800

    v1

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>楊子青Python網頁(Firestore) 電影分級查詢</h1>"
+    homepage = "<h1>楊子青Python網頁(Firestore) 電影分級查詢1</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
     homepage += "<a href=/welcome?nick=tcyang&work=pu>傳送使用者暱稱</a><br>"
